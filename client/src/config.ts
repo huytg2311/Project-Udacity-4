15e34de463c0a67955a15cea4e7134880cf639fc
huytg2311 <huytg23112001@gmail.com>
2024-03-04 12:01:48 +0700

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'ev5br5o9ma';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-uxevvwg15jre2nbf.us.auth0.com',
+  clientId: 'l1xcDLkM4uCTFfqf8fkTiEtrop2dCiSy',
+  callbackUrl: 'http://localhost:3000/callback'
+};
